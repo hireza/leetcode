13b96c2505cdb6cb074d2cf21d16ba96884b645b
Muhammad Reza Pahlevi <mr.mhd.reza@gmail.com>
2024-12-31 17:16:33 +0700

    New: Add Problem-Set/00014-Longest-Common-Prefix

---
+++ Problem-Set/00014-Longest-Common-Prefix/main.go
@@ -0,0 +1,20 @@
+package main
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+
+	for _, str := range strs[1:] {
+		for len(str) < len(prefix) || str[:len(prefix)] != prefix {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+
+	return prefix
+}
